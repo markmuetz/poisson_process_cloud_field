01a052c00f608b3366f49de75620480b0746f556
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-01-16 16:22:23 +0000

    Add a simple inhibition model:
    If a new cloud is less than MIN_SEP from an existing cloud, there is a probability (based on its
    sep) that it will not be created.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import pylab as plt
 LX = 100
 LY = 100
 
+MIN_SEP = 10
+
 
 class Cloud(object):
     def __init__(self, x, y, age, max_age):
@@ -17,31 +19,33 @@ class Cloud(object):
         self.max_age = max_age
 
 
+def calc_min_dist(cloud, test_cloud):
+    min_dist = 1e99
+    for ii in [-1, 0, 1]:
+        for jj in [-1, 0, 1]:
+            x = test_cloud.x + ii * LX
+            y = test_cloud.y + jj * LY
+            dist = np.sqrt((cloud.x - x)**2 + (cloud.y - y)**2)
+            min_dist = min(dist, min_dist)
+    return min_dist
+
 def calc_cloud_stats(clouds, use_min_dist=True):
     dists = []
     for i in range(len(clouds)):
         cloud = clouds[i]
         for j in range(i + 1, len(clouds)):
             test_cloud = clouds[j]
-            min_dist = 1e99
-            for ii in [-1, 0, 1]:
-                for jj in [-1, 0, 1]:
-                    x = test_cloud.x + ii * LX
-                    y = test_cloud.y + jj * LY
-                    dist = np.sqrt((cloud.x - x)**2 + (cloud.y - y)**2)
-                    if use_min_dist:
-                        min_dist = min(dist, min_dist)
-                    else:
-                        dists.append(dist)
-            if use_min_dist:
-                dists.append(min_dist)
+            min_dist = calc_min_dist(cloud, test_cloud)
+            dists.append(min_dist)
     return dists
 
 
 def show_cloud_field(clouds, dists):
     plt.figure(1)
     plt.clf()
+    print(len(clouds))
     for cloud in clouds:
+        print(cloud)
         plt.plot(cloud.x, cloud.y, 'bo')
 
     plt.xlim((0, LX))
@@ -60,14 +64,26 @@ def show_cloud_field(clouds, dists):
     plt.pause(0.01)
 
 
-def create_clouds(mu=3):
-    clouds = []
+def create_clouds(clouds, mu=3):
+    #clouds = []
     n_cloud = sp.random.poisson(mu)
     for i in range(n_cloud):
+        #print(i)
         x = np.random.random() * LX
         y = np.random.random() * LY
         max_age = 3
-        clouds.append(Cloud(x, y, 0, max_age))
+        cloud = Cloud(x, y, 0, max_age)
+        too_close = False
+        for test_cloud in clouds:
+            #print(test_cloud)
+            min_dist = calc_min_dist(cloud, test_cloud)
+            if min_dist < MIN_SEP:
+                if min_dist / MIN_SEP < np.random.random():
+                    too_close = True
+                    break
+        if too_close:
+            continue
+        clouds.append(cloud)
     return clouds
 
 
@@ -82,8 +98,7 @@ def main(nt=100, mu=3.):
             if cloud.age > cloud.max_age:
                 # print('Killing cloud')
                 clouds.remove(cloud)
-        new_clouds = create_clouds(mu)
-        clouds.extend(new_clouds)
+        clouds = create_clouds(clouds, mu)
         new_dists = calc_cloud_stats(clouds)
         # show_cloud_field(clouds, new_dists)
         dists.extend(new_dists)
@@ -92,4 +107,5 @@ def main(nt=100, mu=3.):
 
 
 if __name__ == '__main__':
+    plt.ion()
     dists = main(int(sys.argv[1]), float(sys.argv[2]))
